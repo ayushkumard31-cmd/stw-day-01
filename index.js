95f999a3d7da19c51bcd9516c4d2bb5d8220dc34
Ayush Kumar Das <ayushkumard31@gmail.com>
2026-07-26 15:21:10 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+console.log("Hello,world!");
+console.log(32);
+console.log("The value of 32 "+32);
+console.log(5==0);
+console.log(4+7);
+console.log(`Hello Ayush,
+      I am glad to see you.`);
+console.log('5'+'3');
+console.log('5'-'3');
+console.log('5'==5);// simple compare it
+console.log('5'===5);//compare with datatype
+console.log(`This is a text,this is sum of 2+3: ${2+3}`);
+const name='Grurkulam STW'
+console.log('Hello',name,'is my name')
+let a=3
+let b=2
+console.log(a+b);
+a=10
+console.log(a+b);
+console.log(typeof a);
+console.log(typeof name);
+
+const arr=[3,6,9,0,5];
+console.log(arr);
+console.log(arr[0]);
+const input=prompt("Enter your name");
+console.log("Your name is",input);
+const email=prompt("Enter your email");
+console.log("Your emil is",email);
+console.log(typeof arr);
+const obj={
+    name:{
+    first:"Ayush",
+    last:"kumar"
+    },
+    roll :8,
+    isStudent:true
+
+}
+console.log(typeof obj==typeof arr);
+const abc=null;
+const xyz=undefined
+console.log(abc);
+console.log(abc===xyz);
+console.log(abc==xyz);
+console.log(obj.name.first);
+console.log(obj.school.l);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
